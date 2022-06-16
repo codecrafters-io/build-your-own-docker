a529451760b87d1fb371ee8cd79882c802c1cda0
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-16 22:56:33 +0100

    Add stage 3 solution

---
+++ solutions/go/exit_code/code/app/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+func main() {
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		os.Exit(exitErr.ExitCode()) // The program exited with a non-zero exit code
+	} else if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+}
