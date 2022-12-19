6deaaddbfc22a807d59837395776e7981e1350fb
ThetaSinner <greatdjonfire@hotmail.co.uk>
2022-12-19 13:23:17 +0000

    Adjust error handling in the starter

--- solutions/rust/01-init/code/src/main.rs
+++ solutions/rust/01-init/code/src/main.rs
@@ -1,17 +1,26 @@
+use anyhow::{Context, Result};
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
-        .unwrap();
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
 
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        println!("{}", std_out)
+        let std_out = std::str::from_utf8(&output.stdout)?;
+        println!("{}", std_out);
     } else {
         std::process::exit(1);
     }
+
+    Ok(())
 }
