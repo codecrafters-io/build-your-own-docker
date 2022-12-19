530e5d3d31ddfc4179c31306e77dd37b8bb0efa4
ThetaSinner <greatdjonfire@hotmail.co.uk>
2022-12-19 13:45:54 +0000

    Match the starter and the readme to the updated init

--- compiled_starters/docker-starter-rust/src/main.rs
+++ compiled_starters/docker-starter-rust/src/main.rs
@@ -1,5 +1,7 @@
+use anyhow::{Context, Result};
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -10,12 +12,19 @@ fn main() {
     // let output = std::process::Command::new(command)
     //     .args(command_args)
     //     .output()
-    //     .unwrap();
+    //     .with_context(|| {
+    //         format!(
+    //             "Tried to run '{}' with arguments {:?}",
+    //             command, command_args
+    //         )
+    //     })?;
     //
     // if output.status.success() {
-    //     let std_out = std::str::from_utf8(&output.stdout).unwrap();
-    //     println!("{}", std_out)
+    //     let std_out = std::str::from_utf8(&output.stdout)?;
+    //     println!("{}", std_out);
     // } else {
     //     std::process::exit(1);
     // }
+
+    Ok(())
 }
--- solutions/rust/01-init/explanation.md
+++ solutions/rust/01-init/explanation.md
@@ -10,11 +10,16 @@ let command_args = &args[4..];
 let output = std::process::Command::new(command)
     .args(command_args)
     .output()
-    .unwrap();
+    .with_context(|| {
+        format!(
+            "Tried to run '{}' with arguments {:?}",
+            command, command_args
+        )
+    })?;
 
 if output.status.success() {
-    let std_out = std::str::from_utf8(&output.stdout).unwrap();
-    println!("{}", std_out)
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    println!("{}", std_out);
 } else {
     std::process::exit(1);
 }
