10d1608f2dc882dfd21038d38068d13549bb4e07
Ryan Gang <ryan-gg@outlook.com>
2024-05-30 17:40:05 +0530

    CC-1248: Disable output buffering for both stdout and stderr

--- compiled_starters/c/app/main.c
+++ compiled_starters/c/app/main.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[]) {
 	// Disable output buffering
 	setbuf(stdout, NULL);
+ 	setbuf(stderr, NULL);
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
